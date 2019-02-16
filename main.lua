30c48b697da12dc69ae0ccd1cafed32a760a1c1d
Sarge <sercan.altundas@gmail.com>
2019-02-16 16:35:31 +0200

    debugging player states and moving code to player.lua

--- main.lua
+++ main.lua
@@ -1,13 +1,12 @@
 require 'source.settings'
+require 'source.states'
+require 'source.debug'
+require 'source.states'
 
 local _anim = require 'plugins.anim8'
 local _camera = require 'plugins.Camera'
 local _input = require 'plugins.Input'
 local _player = require 'source.player'
-
-local dirs = {up = 'up', down = 'down', left = 'left', right = 'right' }
-local currentDir = ''
-local hit = false
 
 function love.load()
   love.graphics.setDefaultFilter('nearest', 'nearest')
@@ -16,19 +15,10 @@ function love.load()
   camera = _camera(160, 100, 320, 200)
   camera:setFollowLerp(0.1)
   camera:setFollowStyle('TOPDOWN')
-
   camera.draw_deadzone = true
-
-  player = _player()
-
+  
   input = _input()
-
-  --key bindings
-  input:bind('w', 'move_u' )
-  input:bind('a', 'move_l')
-  input:bind('s', 'move_d')
-  input:bind('d', 'move_r')
-  input:bind('space', 'hit')
+  player = _player()
 
   --canvas
   canvas = love.graphics.newCanvas(960, 200)
@@ -39,13 +29,6 @@ function love.update(dt)
   
   camera:update(dt)
   camera:follow(player.x, player.y)
-
-  if input:down('move_u') then move(dirs.up, dt) end
-  if input:down('move_l') then move(dirs.left, dt) end
-  if input:down('move_d') then move(dirs.down, dt) end
-  if input:down('move_r') then move(dirs.right, dt) end
-  if input:pressed('hit') then hit = true end
-  if input:released('hit') then hit = false end
 end
 
 function love.draw()
@@ -68,30 +51,5 @@ function love.draw()
   love.graphics.draw(canvas, 0, 0, 0, SCALE, SCALE)
   love.graphics.setBlendMode('alpha')
   
-  lg.print(player.cx, 1, 1)
-end
-
-function move(dir, dt)
-  currentDir = dir
-
-  if currentDir == dirs.left and player.cx < 150 then return end 
-  if currentDir == dirs.right and player.cx > 860 then return end 
-  if currentDir == dirs.up and player.cy < 0 then return end 
-  if currentDir == dirs.down and player.cy < 0 then return end 
-
-  if dir == dirs.up then
-    player.y = player.y - dt * player.speed;
-  elseif dir == dirs.down then
-    player.y = player.y + dt * player.speed;
-  elseif dir == dirs.right then
-    player.x = player.x + dt * player.speed;
-  elseif dir == dirs.left then
-    player.x = player.x - dt * player.speed;
-  end
-end
-
-function love.keypressed(key)
-  if key == 'e' then
-      camera:shake(8, 1, 60)
-  end
+  debug()
 end
--- source/player.lua
+++ source/player.lua
@@ -1,10 +1,38 @@
 local Player = {}
 Player.__index = Player
 
+local function move(self, dir, dt)
+  self.state = WalkState.walk
+  self.direction = dir
+
+  if dir == Directions.left and self.cx < 150 then return end 
+  if dir == Directions.right and self.cx > 860 then return end 
+  if dir == Directions.up and self.cy < 0 then return end 
+  if dir == Directions.down and self.cy > 200 then return end 
+
+  if dir == Directions.up then
+    self.y = self.y - dt * self.speed;
+  elseif dir == Directions.down then
+    self.y = self.y + dt * self.speed;
+  elseif dir == Directions.right then
+    self.x = self.x + dt * self.speed;
+  elseif dir == Directions.left then
+    self.x = self.x - dt * self.speed;
+  end
+end
+
 function Player.new()
     local image = love.graphics.newImage('graphics/player.png')
     local width = image:getWidth()
     local height = image:getHeight()
+
+    --key bindings
+    input:bind('up',    'move_u' )
+    input:bind('left',  'move_l')
+    input:bind('down',  'move_d')
+    input:bind('right', 'move_r')
+    input:bind('z',     'drag')
+    input:bind('x',     'hit')
 
     return setmetatable({
       img = image,
@@ -14,17 +42,46 @@ function Player.new()
       y = 100,
       speed = 100,
       cx = 0,
-      cy = 0
+      cy = 0,
+      state = WalkState.idle,
+      direction = Directions.right,
+      hit = false,
+      debug = false
     }, Player)
 end
 
 function Player:update(dt)
   self.cx = self.x + self.w / 2;
   self.cy = self.y + self.h / 2;
+
+  self.state = WalkState.idle
+  self.hit = false
+
+  --move directions
+  if input:down('move_u') then move(self, Directions.up, dt) end
+  if input:down('move_l') then move(self, Directions.left, dt) end
+  if input:down('move_d') then move(self, Directions.down, dt) end
+  if input:down('move_r') then move(self, Directions.right, dt) end
+
+  --dragging weapon
+  if input:down('drag') then 
+    self.state = WalkState.drag 
+    self.speed = 60
+  end
+  if input:released('drag') then self.speed = 100 end
+
+  --hitting
+  if input:down('hit', 0.5, 0.5) then self.hit = true end
 end
 
 function Player:draw()
   lg.draw(self.img, self.x - self.w / 2, self.y - self.h / 2)
+
+  if self.debug and self.hit then
+    lg.setColor(1, 0, 0)
+    lg.rectangle("line", self.x - self.w / 2 + self.w, self.y - self.h / 2, self.w, self.h)
+    lg.setColor(1, 1, 1)
+  end
 end
 
 return setmetatable({}, {__call = function(_, ...) return Player.new(...) end})
