621bbffa766c6456b3687031988e8d9fe5446915
Sarge <sercan.altundas@gmail.com>
2019-02-16 16:34:49 +0200

    game states created

---
+++ source/states.lua
@@ -0,0 +1,18 @@
+SceneState = {
+  menu = 0,
+  game = 1,
+  cred = 2
+}
+
+WalkState = {
+	idle  = 'idle',
+	walk  = 'walk',
+	drag 	= 'drag'
+}
+
+Directions = {
+  up = 'up', 
+  down = 'down', 
+  left = 'left', 
+  right = 'right' 
+}
