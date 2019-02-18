4d3fce9a37e25fae7d70aa2121147933f831b7f8
Sarge <sercan.altundas@gmail.com>
2019-02-18 02:20:00 +0200

    menu and instructions

--- main.lua
+++ main.lua
@@ -21,34 +21,60 @@ function love.load()
   horde = _horde()
   map = _map()
 
+  gameState = SceneStates.menu
+
+  menu = lg.newImage('graphics/menu/title.png')
+  intro = lg.newImage('graphics/menu/intro.png')
+  
+  input:bind('space', 'skip')
+
   --canvas
   canvas = love.graphics.newCanvas(960, 200)
 end
 
 function love.update(dt)
-  map:update(dt)
-  player:update(dt)
-  horde:update(dt)
-  camera:update(dt)
-  camera:follow(player.x, player.y)
+  if gameState == SceneStates.menu then
+    if input:pressed('skip') then
+      gameState = SceneStates.intro
+    end
+  elseif gameState == SceneStates.intro then
+    if input:pressed('skip') then
+      gameState = SceneStates.game
+    end
+  elseif gameState == SceneStates.game then
+    map:update(dt)
+    player:update(dt)
+    horde:update(dt)
+    camera:update(dt)
+    camera:follow(player.x, player.y)
+  end
 end
 
 function love.draw()
   love.graphics.setCanvas(canvas)
   love.graphics.clear()
-  
-  camera:attach()
-  
-  map:draw()
-  horde:draw(false)
-  player:draw()
-  horde:draw(true)
 
-  debugCanvas()
-
-  camera:detach()
-  camera:draw()
+  if gameState == SceneStates.menu then
+    lg.draw(menu, 0, 0)
+  elseif gameState == SceneStates.intro then
+    lg.draw(intro, 0, 0)
+  elseif gameState == SceneStates.game then
     
+    camera:attach()
+    
+    map:draw()
+    horde:draw(false)
+    player:draw()
+    horde:draw(true)
+
+    --debugCanvas()
+
+    camera:detach()
+    camera:draw()
+
+    map:drawBars()
+  end
+  
   -- Draw the 400x300 canvas scaled by 2 to a 800x600 screen
   love.graphics.setCanvas()
   love.graphics.setColor(1, 1, 1, 1)
@@ -56,5 +82,5 @@ function love.draw()
   love.graphics.draw(canvas, 0, 0, 0, SCALE, SCALE)
   love.graphics.setBlendMode('alpha')
   
-  debug()
+  --debug()
 end
